3db4294abb51774b2d5e58942275886eefc7ad48
Sanrai10 <121992430+Sanrai10@users.noreply.github.com>
2023-04-02 03:09:05 -0700

    BackGround Music
    In this mechanic, the object jump when pressing the space key.

---
+++ Background music/Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    private bool isGrounded = true;
+    public float jumpForce = 15f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 velocity = rb.velocity;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -speed;
+        }
+
+        //space to jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = jumpForce;
+            isGrounded = false;
+        }
+
+        rb.velocity = velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
